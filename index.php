fde4f5e1c39ae10c9a1ec49eb3de371dc06f6878
Fabio Calazans <calazansbartender@hotmail.com>
2019-12-25 11:37:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php 
-echo "Hello World";
-?>
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWorldController
+{
+    /**
+     * @Route("/hello")
+     */
+    public function getHelloWorld(Request $request)
+    {
+        $name = $request->get('name');
+
+        return new Response('Hello '.$name.'!');
+    }
+}
