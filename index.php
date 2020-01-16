fc5ae5c19347ea8b5e9e4fd8120c10048be3dbfc
Fabio Calazans <calazansbartender@hotmail.com>
2020-01-16 19:19:40 +0000

    Last edit

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test php</title>
+</head>
+<body>
 <?php
-
-namespace App\Controller;
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-class HelloWorldController
-{
-    /**
-     * @Route("/hello")
-     */
-    public function getHelloWorld(Request $request)
-    {
-        $name = $request->get('name');
-
-        return new Response('Hello '.$name.'!');
-    }
-}
+$today = getdate();
+echo $today["month"], $today["year"]
+?>
+</body>
+</html>
